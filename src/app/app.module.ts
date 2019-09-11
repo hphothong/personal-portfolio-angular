284ae70be6f72aafb582b2d6eaacddea1b772b7e
hphothong <hphothong@gmail.com>
2019-09-10 18:35:20 -0600

    Adding navbar and footer component skeletons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CodingChallengesPageComponent } from './pages/coding-challenges-page/coding-challenges-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, CodingChallengesPageComponent],
+  declarations: [AppComponent, HomePageComponent, CodingChallengesPageComponent, NavbarComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
