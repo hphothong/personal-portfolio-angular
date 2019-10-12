dea3714a72114c285000b2729d37e83eeb13211b
hphothong <hphothong@gmail.com>
2019-10-11 22:04:24 -0600

    Adding customizable jumbotron

--- src/app/components/jumbotron/jumbotron.component.ts
+++ src/app/components/jumbotron/jumbotron.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-jumbotron',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jumbotron.component.scss']
 })
 export class JumbotronComponent implements OnInit {
+  @Input()
+  private title: string;
 
-  constructor() { }
+  @Input()
+  private subtitle: string;
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
