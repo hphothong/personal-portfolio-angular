68fde00a09750114cc9dc871d52e29c611db7e5d
hphothong <hphothong@gmail.com>
2019-10-11 21:19:21 -0600

    fixing unit tests

--- src/app/models/github/github-gist-file.spec.ts
+++ src/app/models/github/github-gist-file.spec.ts
@@ -2,6 +2,14 @@ import { GithubGistFile } from './github-gist-file';
 
 describe('GithubGistFile', () => {
   it('should create an instance', () => {
-    expect(new GithubGistFile()).toBeTruthy();
+    expect(
+      new GithubGistFile({
+        filename: 'test',
+        type: 'test',
+        language: 'test',
+        rawUrl: 'test',
+        size: 1
+      })
+    ).toBeTruthy();
   });
 });
--- src/app/models/github/github-gist-model.spec.ts
+++ src/app/models/github/github-gist-model.spec.ts
@@ -2,6 +2,15 @@ import { GithubGistModel } from './github-gist-model';
 
 describe('GithubGistModel', () => {
   it('should create an instance', () => {
-    expect(new GithubGistModel()).toBeTruthy();
+    expect(
+      new GithubGistModel({
+        url: 'test',
+        files: [],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        description: 'test',
+        htmlUrl: 'test'
+      })
+    ).toBeTruthy();
   });
 });
